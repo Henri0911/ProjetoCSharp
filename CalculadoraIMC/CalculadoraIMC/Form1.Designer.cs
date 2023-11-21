9a5f74aa24559bba5e7a28ea84695be1db48c759
Henri0911 <helio_h9@hotmail.com>
2023-11-21 19:14:46 -0300

    Calculadora IMC

---
+++ CalculadoraIMC/CalculadoraIMC/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace CalculadoraIMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.labelMensagem = new System.Windows.Forms.Label();
+            this.textAltura = new System.Windows.Forms.TextBox();
+            this.textPeso = new System.Windows.Forms.TextBox();
+            this.buttonCalc = new System.Windows.Forms.Button();
+            this.buttonLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(70, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Altura";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(70, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Peso";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(70, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Resultado";
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.AutoSize = true;
+            this.labelResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResultado.Location = new System.Drawing.Point(220, 200);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(33, 25);
+            this.labelResultado.TabIndex = 3;
+            this.labelResultado.Text = "---";
+            // 
+            // labelMensagem
+            // 
+            this.labelMensagem.AutoSize = true;
+            this.labelMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMensagem.Location = new System.Drawing.Point(220, 270);
+            this.labelMensagem.Name = "labelMensagem";
+            this.labelMensagem.Size = new System.Drawing.Size(33, 25);
+            this.labelMensagem.TabIndex = 4;
+            this.labelMensagem.Text = "---";
+            // 
+            // textAltura
+            // 
+            this.textAltura.Location = new System.Drawing.Point(175, 55);
+            this.textAltura.Name = "textAltura";
+            this.textAltura.Size = new System.Drawing.Size(131, 22);
+            this.textAltura.TabIndex = 5;
+            // 
+            // textPeso
+            // 
+            this.textPeso.Location = new System.Drawing.Point(175, 110);
+            this.textPeso.Name = "textPeso";
+            this.textPeso.Size = new System.Drawing.Size(131, 22);
+            this.textPeso.TabIndex = 6;
+            // 
+            // buttonCalc
+            // 
+            this.buttonCalc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCalc.Location = new System.Drawing.Point(100, 350);
+            this.buttonCalc.Name = "buttonCalc";
+            this.buttonCalc.Size = new System.Drawing.Size(102, 53);
+            this.buttonCalc.TabIndex = 7;
+            this.buttonCalc.Text = "Calcular";
+            this.buttonCalc.UseVisualStyleBackColor = true;
+            this.buttonCalc.Click += new System.EventHandler(this.buttonCalc_Click);
+            // 
+            // buttonLimpar
+            // 
+            this.buttonLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLimpar.Location = new System.Drawing.Point(300, 350);
+            this.buttonLimpar.Name = "buttonLimpar";
+            this.buttonLimpar.Size = new System.Drawing.Size(102, 53);
+            this.buttonLimpar.TabIndex = 8;
+            this.buttonLimpar.Text = "Limpar";
+            this.buttonLimpar.UseVisualStyleBackColor = true;
+            this.buttonLimpar.Click += new System.EventHandler(this.buttonLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 450);
+            this.Controls.Add(this.buttonLimpar);
+            this.Controls.Add(this.buttonCalc);
+            this.Controls.Add(this.textPeso);
+            this.Controls.Add(this.textAltura);
+            this.Controls.Add(this.labelMensagem);
+            this.Controls.Add(this.labelResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Calculadora IMC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelResultado;
+        private System.Windows.Forms.Label labelMensagem;
+        private System.Windows.Forms.TextBox textAltura;
+        private System.Windows.Forms.TextBox textPeso;
+        private System.Windows.Forms.Button buttonCalc;
+        private System.Windows.Forms.Button buttonLimpar;
+    }
+}
+
---
+++ CalculadoraIMC/CalculadoraIMC/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraIMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalc_Click(object sender, EventArgs e)
+        {
+            float altura, peso, media;
+            bool verifica;
+
+            verifica = float.TryParse(textAltura.Text, out altura);
+            if (verifica == false)
+            {
+                MessageBox.Show("Formato de valor inválido na Altura.");
+                return;
+            }
+            verifica = float.TryParse(textPeso.Text, out peso);
+            if (verifica == false)
+            {
+                MessageBox.Show("Formato de valor inválido na Peso.");
+                return;
+            }
+
+            altura = float.Parse(textAltura.Text);
+
+            peso = float.Parse(textPeso.Text);
+
+            media = peso / (altura * altura);
+
+            labelResultado.Text = string.Format("IMC: {0}", Math.Round(media, 2));
+
+            if (media < 17)
+            {
+                labelMensagem.Text = string.Format("Muito abaixo do peso");
+            }
+            else if (media >= 17 && media < 18.49) 
+            {
+                labelMensagem.Text = string.Format("Abaixo do peso");
+            }
+            else if (media >= 18.50 && media < 24.99)
+            {
+                labelMensagem.Text = string.Format("Peso normal");
+            }
+            else if (media >= 25 && media < 29.99)
+            {
+                labelMensagem.Text = string.Format("Acima do peso");
+            }
+            else if (media >= 30 && media < 34.99)
+            {
+                labelMensagem.Text = string.Format("Obesidade grau I");
+            }
+            else if (media >= 35 && media < 40)
+            {
+                labelMensagem.Text = string.Format("Obesidade grau II");
+            }
+            else
+            {
+                labelMensagem.Text = string.Format("Obesidade grau III");
+            }
+
+        }
+        private void buttonLimpar_Click(object sender, EventArgs e)
+        {
+            textAltura.Text = "";
+            textPeso.Text = "";
+            labelResultado.Text = "---";
+            labelMensagem.Text = "---";
+        }
+    }
+}
